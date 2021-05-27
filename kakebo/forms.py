891df86a92b29a0a2fc20e7c444f199fcc7e26ce
yalaska04 <79913491+yalaska04@users.noreply.github.com>
2021-05-27 16:22:40 +0200

    Inserción 2: falta gestión de errores

--- kakebo/forms.py
+++ kakebo/forms.py
@@ -1,12 +1,17 @@
-
 from flask_wtf import FlaskForm
-from wtforms import  DateField, StringField, SelectField, SubmitField, FloatField, BooleanField
-from wtforms.validators import DataRequired, Length
+from wtforms import DateField, StringField, SelectField, SubmitField, FloatField, BooleanField
+from wtforms.validators import DataRequired, Length, ValidationError
+from datetime import date
+
+def fecha_por_debajo_de_hoy(formulario, campo):
+    hoy = date.today()
+    if campo.data > hoy:
+        raise ValidationError('La fecha {} no puede ser mayor que {}'.format(campo.data, hoy))
 
 class MovimientosForm(FlaskForm):
-    fecha = DateField("Fecha", validators = [DataRequired()])
+    fecha = DateField("Fecha", validators = [DataRequired(message="Debe informar una fecha válida"), fecha_por_debajo_de_hoy])
     concepto = StringField("Concepto", validators = [DataRequired(), Length(min=10)])
-    categoria = SelectField("Categoria", choices=[('SU', 'Supervivencia'), ('OV', 'Ocio/Vicio'), 
+    categoria = SelectField("Categoria", choices=[('00', ''),('SU', 'Supervivencia'), ('OV', 'Ocio/Vicio'), 
                             ('CU', 'Cultura'), ('EX', 'Extras')])
     cantidad = FloatField("Cantidad", validators = [DataRequired()])
     esGasto = BooleanField("Es gasto")
--- kakebo/views.py
+++ kakebo/views.py
@@ -1,5 +1,5 @@
 from kakebo import app
-from flask import jsonify, render_template
+from flask import jsonify, render_template, request, redirect, url_for
 from kakebo.forms import MovimientosForm
 
 import sqlite3
@@ -33,6 +33,41 @@ def index():
 
 @app.route('/nuevo', methods=['GET', 'POST'])
 def nuevo():
-    form = MovimientosForm() 
+    formulario = MovimientosForm() 
 
-    return render_template('alta.html', form = form)
+    if request.method == 'GET': 
+        return render_template('alta.html', form = formulario)
+    else: 
+        if formulario.validate(): 
+            conexion = sqlite3.connect("movimientos.db")
+            cur = conexion.cursor()
+
+            query = "INSERT INTO movimientos (fecha, concepto, categoria, esGasto, cantidad) VALUES (?, ?, ?, ?, ?)"
+            try: 
+                cur.execute(query, [formulario.fecha.data, formulario.concepto.data, formulario.categoria.data,
+                                    formulario.esGasto.data, formulario.cantidad.data]) # inyectamos los valores
+            
+            except sqlite3.Error as el_error: 
+                print('Error en SQL INSERT', el_error)
+                return render_template('alta.html', form= formulario)
+
+            """
+            query = "INSERT INTO movimientos (fecha, concepto, categoria, esGasto, cantidad) VALUES (:fecha, :concepto, :categoria, :esGasto, :cantidad)"
+            cur.execute(query, {
+                'fecha': formulario.fecha.data, 
+                'concepto': formulario.concepto.data, 
+                'categoria': formulario.categoria.data,
+                'esGasto': formulario.esGasto.data, 
+                'cantidad':formulario.cantidad.data
+            }
+            """
+
+            conexion.commit()
+            conexion.close()
+
+            return redirect(url_for('index'))
+
+            # Redirect a la ruta /
+        else: 
+            return render_template('alta.html', form = formulario)
+
